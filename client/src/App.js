1c48d229e186310bbf7cd054536913a3571cf328
Josue Rodriguez <zhippuden@gmail.com>
2020-02-04 12:02:38 +0100

    Apollo Client Added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 ///Components
 import BookList from './Components/BookList';
 
+///Apollo client setup
+const client = new ApolloClient({
+	uri: 'http://localhost:5000/'
+});
+
 const App = () => {
 	return (
-		<div id='main'>
-			<h1>Ninja's Reading List</h1>
-			<BookList />
-		</div>
+		<ApolloProvider client={client}>
+			<div id='main'>
+				<h1>Ninja's Reading List</h1>
+				<BookList />
+			</div>
+		</ApolloProvider>
 	);
 };
 
